79ec5fac3240ff23abb9067f068b2ce667a0ca92
Mr.Promsawat pitakmetakul <>
2021-04-29 11:47:48 +0700

    create lambda spring boot

---
+++ lambda_project/src/main/kotlin/com/example/lambda/lambda_project/LambdaProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.example.lambda.lambda_project
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LambdaProjectApplication
+
+fun main(args: Array<String>) {
+	runApplication<LambdaProjectApplication>(*args)
+}
---
+++ lambda_project/src/test/kotlin/com/example/lambda/lambda_project/LambdaProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.lambda.lambda_project
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LambdaProjectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
