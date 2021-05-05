98bc02cef97056525527c763f1fcead350a15e4f
Mr.Promsawat pitakmetakul <>
2021-05-05 14:20:02 +0700

    [Update] dir part project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "puck-multi-module-sample"
 
 include ("utils_project")
-include ("webflux_project")
+include ("core:webflux_project")
 include ("main_project")
 include ("start_local_project")
 include ("lambda_project")
--- start_local_project/build.gradle.kts
+++ start_local_project/build.gradle.kts
@@ -16,7 +16,7 @@ repositories {
 }
 
 dependencies {
-	implementation(project(":webflux_project"))
+	implementation(project(":core:webflux_project"))
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
